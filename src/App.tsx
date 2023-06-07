8ea46000b16e79dba1415e7defafc0622ab21c91
Andriikryl <andreyk557@gmail.com>
2023-06-07 13:43:26 +0300

    add images for carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-
+const slides = [
+  {
+    title: "One Tap Setup",
+    img: "/slide-img/one-tap.jpeg",
+    imageWidth: 343,
+    imageHeight: 375,
+  },
+  {
+    title: "Personalized Spatial Audio",
+    img: "/slide-img/spatial-audio.jpeg",
+    imageWidth: 211,
+    imageHeight: 375,
+  },
+  {
+    title: "Audio Sharing",
+    img: "/slide-img/audio-sharing.jpeg",
+    imageWidth: 265,
+    imageHeight: 352,
+    centered: true,
+  },
+  {
+    title: "Automatic Switching",
+    img: "/slide-img/automatic-switching.jpeg",
+    imageWidth: 336,
+    imageHeight: 100,
+    centered: true,
+  },
+  {
+    title: "Siri",
+    img: "/slide-img/siri.jpeg",
+    imageWidth: 168,
+    imageHeight: 168,
+    centered: true,
+  },
+  {
+    title: "Accessibility",
+    img: "/slide-img/a11y.jpeg",
+    imageWidth: 135,
+    imageHeight: 135,
+    centered: true,
+  },
+];
 
 function App() {
-  return (
-    <h1>asd</h1>
-  )
+  return <div className=""></div>;
 }
 
-export default App
+export default App;
